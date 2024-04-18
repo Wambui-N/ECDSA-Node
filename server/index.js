ba13f041a2b366890677d042765330528e8a04e8
Wambui Ndungu <wambuin18@gmail.com>
2024-04-18 15:59:05 +0300

    Verify signature

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 //const cors = require("cors");
 import cors from "cors";
 import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
-
+import { hexToBytes, toHex, utf8ToBytes } from "ethereum-cryptography/utils.js";
 
 const app = express();
 const port = 3042;
@@ -12,44 +12,58 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": { balance: 100, privateKey: '', publicKey: '', address: '0x1' },
-  "0x2": { balance: 50, privateKey: '', publicKey: '', address: '0x2' },
-  "0x3": { balance: 75, privateKey: '', publicKey: '', address: '0x3' },
+  "0x1": { balance: 100, privateKey: "", publicKey: "", address: "0x1" },
+  "0x2": { balance: 50, privateKey: "", publicKey: "", address: "0x2" },
+  "0x3": { balance: 75, privateKey: "", publicKey: "", address: "0x3" },
 };
 
 //generate key pair
 for (const address in balances) {
   const privateKeyObject = secp256k1.utils.randomPrivateKey();
-  const privateKey = Object.values(privateKeyObject)
-  .map(value => value.toString(16).padStart(2, '0'))
-  .join('');
+  const privateKey = bytesToHex (privateKeyObject)
   balances[address].privateKey = privateKey;
-  
+
   const publicKeyObject = secp256k1.getPublicKey(privateKey);
-  const publicKey = Object.values(publicKeyObject)
-  .map(value => value.toString(16).padStart(2, '0'))
-  .join('');
+  const publicKey = bytesToHex (publicKeyObject);
   balances[address].publicKey = publicKey;
 }
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
   const balance = balances[address].balance || 0;
-  res.send({ balance });
+  const privateKey = balances[address].privateKey;
+  if (balance[address]) {
+    res.send({ balance, privateKey });
+  } else {
+    res.status(404).send({ message: "Address not found!" });
+  }
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
-
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
-
-  if (balances[sender.balance] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+  const { sender, recipient, amount, signature } = req.body;
+  //verify signature
+  const senderPublicKey = balances[sender].publicKey;
+  const isVerified = secp256k1.verify(
+    hexToBytes(signature),
+    messageHash,
+    hexToBytes(publicKey)
+  );
+  if (!isVerified) {
+    res.status(400).send({ message: "Invalid signature!" });
+    console.log("Invalid signature!");
+    return;
   } else {
-    balances[sender].balance -= amount;
-    balances[recipient].balance += amount;
-    res.send({ balance: balances[sender].balance });
+    console.log("Signature verified!");
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender].balance < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender].balance -= amount;
+      balances[recipient].balance += amount;
+      res.send({ balance: balances[sender].balance });
+    }
   }
 });
 
